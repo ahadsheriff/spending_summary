f11d33b96ed4629bbe05f585e0049e6fa562927f
Ahad Sheriff <ahadsheriff@gmail.com>
2019-03-27 09:16:39 -0400

    working server

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 
 # Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
+# Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
 PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
 PLAID_SECRET = os.getenv('PLAID_SECRET')
 PLAID_PUBLIC_KEY = os.getenv('PLAID_PUBLIC_KEY')
